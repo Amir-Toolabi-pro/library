f3a4e6e86c33484959343294e8db6ecd16cbcee6
Amir-Toolabi-pro <amirooogamer81@gmail.com>
2023-03-05 06:44:25 +0330

    use NavLink for dinamic styling navbars

--- src/BooksMap.jsx
+++ src/BooksMap.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Books } from './Data';
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 
 
@@ -15,9 +15,18 @@ const BooksMap = () => {
       <div style={{ display: "flex"}}>
         <div style={{ display: "flex", flexDirection: "column" , borderLeft:"1px solid black" }}>
           {books.map(theBook => (
-            <Link to={`${theBook.bookNum}`} key={theBook.bookNum}>
+            <NavLink to={`${theBook.bookNum}`} key={theBook.bookNum}
+              style={({isActive})=> {
+                return{
+                  textDecoration: "none",
+
+                  color: isActive? "red" : "black" ,
+                  borderBottom: isActive? "2px solid red" : "none",
+                }
+              }}
+            >
               {theBook.bookName}
-            </Link>
+            </NavLink>
           ))}
         </div>
         <Outlet/>
